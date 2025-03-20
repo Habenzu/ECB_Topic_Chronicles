c60bd446c1bda0c03fb8852f274c9f61f0fd954d
Habenzu <pov.hirschhausen@gmx.de>
2025-03-20 17:39:29 +0100

    changed to lighweight logger and added docu

--- gather_data.py
+++ gather_data.py
@@ -2,69 +2,89 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 import requests
-import logging
 from urllib.parse import urlparse
 from concurrent.futures import ThreadPoolExecutor
 import time
 import random
 from bs4 import BeautifulSoup
-from pathlib import Path
 import re
 from tqdm import tqdm
 import json
-
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
+from loguru import logger
+import sys
 
 FILE_DIR = Path(__file__).resolve().parent
+SCRIPT_NAME = Path(__file__).name
+logger.remove()
+logger.add(sys.stdout, format="{time} | {level} | {module}.{function}:{line} | {message}")
+logger.add("log_file.log", format="{time} | {level} | {module}.{function}:{line} | {message}")
 
-class Metadata(): 
-    def __init__(self, path_to_file = Path(r'data\export_datamart.csv')):
-        self.path_to_file = path_to_file
+class Metadata:
+    """
+    Handles the loading and preprocessing of metadata from a CSV file.
+    """
+    def __init__(self, path_to_file: str|Path = r"data\export_datamart.csv") -> None:
+        self.path_to_file = Path(path_to_file) if isinstance(path_to_file, str) else path_to_file
         self.metadata_df = pd.read_csv(self.path_to_file)
         self._prepare_metadata()
 
-    def _prepare_metadata(self):
-        self.metadata_df['when_speech'] = pd.to_datetime(self.metadata_df['when_speech'], format="%Y-%m-%d")
-        self.metadata_df['file_ending'] = self.metadata_df['what_weblink'].str.extract(r'\.(\w+)$')
+    def _prepare_metadata(self) -> None:
+        """
+        Prepares metadata by formatting dates and extracting file extensions.
+        """
+        self.metadata_df["when_speech"] = pd.to_datetime(
+            self.metadata_df["when_speech"], format="%Y-%m-%d"
+        )
+        self.metadata_df["file_ending"] = self.metadata_df["what_weblink"].str.extract(
+            r"\.(\w+)$"
+        )
         self.what_type_dict = {
-            'S': 'speech',
-            'I': 'interview',
-            'P': 'press-conference',
-            'B': 'blog-post',
-            'E': 'podcast',
+            "S": "speech",
+            "I": "interview",
+            "P": "press-conference",
+            "B": "blog-post",
+            "E": "podcast",
         }
-        self.metadata_df['what_type_long'] = self.metadata_df['what_type'].apply(lambda x: self.what_type_dict[x])
+        self.metadata_df["what_type_long"] = self.metadata_df["what_type"].apply(
+            lambda x: self.what_type_dict[x]
+        )
 
-    def get_metadata(self):
-        return self.metadata_df
+class DataDownloader:
+    """
+    Downloads the speeches/documents from the url from the metadata and saves it to some directory.
+    """
+    def __init__(self, output_dir: str|Path =FILE_DIR / "data" / "speeches", metadata_file: str|Path = r"data\export_datamart.csv") -> None:
+        self.output_dir = Path(output_dir) if isinstance(output_dir, str) else output_dir
+        self.metadata_df = Metadata(metadata_file).metadata_df
+        if not self.output_dir.exists():
+            self.output_dir.mkdir(parents=True, exist_ok=True)
 
-class DataDownloader(): 
-    def __init__(self, output_dir = FILE_DIR / 'data' / 'speeches', metadata_file = Path(r'data\export_datamart.csv')):
-        self.output_dir = output_dir
-        self.metadata_df = Metadata(metadata_file).get_metadata()
-        if not Path(self.output_dir).exists():
-            Path(self.output_dir).mkdir(parents=True, exist_ok=True)
-
-    def _download_file(self, url, file_path):
+    def _download_file(self, url:str, file_path:Path) -> None:
+        """
+        Downloads a file from the given URL and saves it to the specified path.
+        
+        Args:
+            url (str): The URL of the file to download.
+            file_path (Path): The path to save the downloaded file.
+        """
         try:
-            # time.sleep(2)
             time.sleep(abs(random.normalvariate()))
             headers = {
-                'User-Agent': random.choice([
-                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0',
-                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1 Safari/605.1.15',
-                    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
-                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134'
-                ])
+                "User-Agent": random.choice(
+                    [
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0",
+                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1 Safari/605.1.15",
+                        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134",
+                    ]
+                )
             }
             response = requests.get(url, headers=headers, allow_redirects=True)
             response.raise_for_status()
-            
+
             # check if redirected url is the same otherwise change the file path
-            orig_url_suffix = Path(urlparse(url)).suffix 
+            orig_url_suffix = Path(urlparse(url)).suffix
             redirected_url_suffix = Path(urlparse(response.url).path).suffix
             if orig_url_suffix != redirected_url_suffix:
                 file_path = file_path.with_suffix(redirected_url_suffix)
@@ -77,82 +97,124 @@ class DataDownloader():
         except requests.exceptions.RequestException as e:
             logger.error(f"Error downloading {url}: {e}")
 
-
     def download_data(self):
+        """
+        Downloads all document files concurrently.
+        """
         with ThreadPoolExecutor(max_workers=10) as executor:
             for index, row in self.metadata_df.iterrows():
-                speech_id = row['speech_id']
-                url = row['what_weblink']
-                file_ending = row['file_ending']
+                speech_id = row["speech_id"]
+                url = row["what_weblink"]
+                file_ending = row["file_ending"]
                 if file_ending is np.nan:
-                    file_ending = 'pdf'
-                date = row['when_speech'].strftime('%Y-%m-%d')
-                type_long = row['what_type_long']
-                filepath = Path(self.output_dir) / f"{speech_id}_{date}_{type_long}.{file_ending}"
+                    file_ending = "pdf"
+                date = row["when_speech"].strftime("%Y-%m-%d")
+                type_long = row["what_type_long"]
+                filepath = Path(self.output_dir)/ f"{speech_id}_{date}_{type_long}.{file_ending}"
                 executor.submit(self._download_file, url, filepath)
 
-class Parser(): 
-    def __init__(self, speeches_dir = Path('data/speeches'), out_path = Path('data/speeches/parsed.jsonl'), metadata_file = Path(r'data\export_datamart.csv')):
-        self.speeches_dir = speeches_dir
-        self.out_path = out_path
-        self.classes_to_remove = ["ecb-publicationDate", "ecb-authors", "ecb-pressContentSubtitle"]
+class Parser:
+    """
+    Parses HTML speech files, extracting main content and saving it as JSON.
+    """
+    def __init__(self, speeches_dir: str|Path = Path("data/speeches"), out_path: str|Path =Path("data/speeches/parsed.jsonl"), metadata_file: str|Path =Path(r"data\export_datamart.csv")) -> None:
+        """
+        Initializes the Parser.
+
+        Args:
+            speeches_dir (Path): Directory containing speech HTML files.
+            out_path (Path): Path for the parsed JSON output.
+            metadata_file (Path): Path to metadata file.
+        """
+        self.speeches_dir = Path(speeches_dir) if isinstance(speeches_dir, str) else speeches_dir
+        self.out_path = Path(out_path) if isinstance(out_path, str) else out_path
+        self.classes_to_remove = [
+            "ecb-publicationDate",
+            "ecb-authors",
+            "ecb-pressContentSubtitle",
+        ]
         # If the following text is found the parser stops processing the remaining paragraphs
         self.texts_to_remove = [
             "The views expressed in each blog entry are those of the author(s) and do not necessarily represent the views of the European Central Bank and the Eurosystem.",
         ]
-        self.div_classes_to_remove = ["related-topics", "title", "address-box", "footnotes", "final hidden"]
-        self.metadata_df = Metadata(metadata_file).get_metadata()
+        self.div_classes_to_remove = [
+            "related-topics",
+            "title",
+            "address-box",
+            "footnotes",
+            "final hidden",
+        ]
+        self.metadata_df = Metadata(metadata_file).metadata_df
 
-    def extract_main_content_from_html(self, html_file_path):
+    def extract_main_content_from_html(self, html_file_path:str|Path) -> dict:
+        """
+        Extracts the main content from an HTML file.
+        
+        Args:
+            html_file_path (str|Path): Path to the HTML file.
+        Returns:
+            dict: A dictionary containing the main content and related topics.
+        """
         with open(html_file_path, "r", encoding="utf-8") as f:
             html_content = f.read()
-        soup = BeautifulSoup(html_content, 'html.parser')
-        main_content = soup.find('main')
+        soup = BeautifulSoup(html_content, "html.parser")
+        main_content = soup.find("main")
         result = {"content": "", "related_topics": []}
-    
+
         # Extract related topics, has to be done before the rest otherwise its eaten from the soup already
-        related_topics_div = soup.find('div', class_='related-topics')
+        related_topics_div = soup.find("div", class_="related-topics")
         if related_topics_div:
-            result["related_topics"] = [li.get_text(strip=True) for li in related_topics_div.find_all('li')]
+            result["related_topics"] = [
+                li.get_text(strip=True) for li in related_topics_div.find_all("li")
+            ]
         if main_content:
             # Remove paragraphs with the specified classes
             for class_to_remove in self.classes_to_remove:
-                for tag in main_content.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'], class_=class_to_remove):
+                for tag in main_content.find_all(
+                    ["p", "h1", "h2", "h3", "h4", "h5", "h6"], class_=class_to_remove
+                ):
                     tag.decompose()
-        
+
             # Remove divs with the specified classes
             for div_class_to_remove in self.div_classes_to_remove:
-                for div in main_content.find_all('div', class_=div_class_to_remove):
+                for div in main_content.find_all("div", class_=div_class_to_remove):
                     div.decompose()
-                    
+
             # Remove paragraphs containing specific text and stop processing further paragraphs
             stop_processing = False
-            for p in main_content.find_all('p'):
+            for p in main_content.find_all("p"):
                 if stop_processing:
                     p.decompose()
-                elif any(text_to_remove in p.get_text() for text_to_remove in self.texts_to_remove):
+                elif any(
+                    text_to_remove in p.get_text()
+                    for text_to_remove in self.texts_to_remove
+                ):
                     p.decompose()
                     stop_processing = True
-        
-            # Extract text from the main content
-            content = main_content.get_text(separator='\n', strip=True)
-            # Remove multiple whitespaces and linebreaks 
-            content = re.sub(r'\s+', ' ', content)
+
+            content = main_content.get_text(separator="\n", strip=True)
+            # Remove multiple whitespaces and linebreaks
+            content = re.sub(r"\s+", " ", content)
             result["content"] = content
-    
+
         return result
-    
-    def process_directory(self):
+
+    def process_directory(self) -> None:
+        """
+        Processes all HTML files in the speeches directory and saves the extracted content as JSON.
+        """
         if self.out_path.exists():
             logger.info(f"File {self.out_path} already exists, skipping processing.")
             return None
 
         results = []
         html_files = list(Path(self.speeches_dir).rglob("*.html"))
-        for index, html_file in enumerate(tqdm(html_files, desc="Processing HTML files"), start=1):
-            speech_id, date, type_long = html_file.stem.split('_')
+        for index, html_file in enumerate(
+            tqdm(html_files, desc="Processing HTML files"), start=1
+        ):
+            speech_id, date, type_long = html_file.stem.split("_")
             speech_id = int(speech_id)
-            
+
             result = self.extract_main_content_from_html(html_file)
             result["speech_id"] = speech_id
             result["date"] = date
@@ -174,13 +236,13 @@ class Parser():
 
 
 if __name__ == "__main__":
-    logger.setLevel(logging.DEBUG)
-    if not Path('data/speeches').is_dir() and len(list(Path('data/speeches').glob("*.html"))) >= 3800: 
-        data_downloader = DataDownloader(output_dir=FILE_DIR / 'data' / 'speeches')
-        data_downloader.download_data()
-    
-    parser = Parser()
-    parser.process_directory()
+    logger.info("Starting data gathering process")
+    logger.debug("Downloading data")
     pass
+    # if not Path('data/speeches').is_dir() and len(list(Path('data/speeches').glob("*.html"))) >= 3800: 
+    #     data_downloader = DataDownloader(output_dir=FILE_DIR / 'data' / 'speeches')
+    #     data_downloader.download_data()
 
-
+    # parser = Parser()
+    # parser.process_directory()
+    # pass
